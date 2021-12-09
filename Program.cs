c2da66296d968e1ceefade0b5d6320d594dad877
Oleksandr Kolodchenko <oleksandrkolodchenko@gmail.com>
2021-12-09 13:23:40 +0200

    Create Hello World Program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿NewMethod();
+
+void NewMethod()
+{
+    Console.WriteLine("What is your name?");
+    string UserName;
+    UserName = Console.ReadLine();
+
+    Console.WriteLine("Hello, " + UserName);
+     
+       
+    Console.Read ();
+}
